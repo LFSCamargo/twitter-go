fa45538c09422ccb5e6433c9799c8156d60435d9
Luiz Fernando <luizepauloxd@gmail.com>
2020-12-25 21:58:10 -0300

    feat: adds comments to the constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,15 +1,22 @@
 package constants
 
+// TweetDeleted Mutation success message
 const TweetDeleted = "The tweet has been deleted"
 
+// InsuficientPermissions Error message when a user does not have permissions to do someaction
+const InsuficientPermissions = "Insuficient Permissions"
+
+// InternalServerError Error message when the server got a unexpected error
 const InternalServerError = "Internal Server Error"
 
+// NotFound Error message when something was not found
 const NotFound = "Not Found"
 
+// WrongEmailOrPassword Error message when the user mismatch the password or email
 const WrongEmailOrPassword = "Wrong email or password"
 
+// UserAlreadyRegistered Error message when the user tries to add a same email user
 const UserAlreadyRegistered = "User already registered"
 
-const UnexpectedError = "An unexpected error occurred"
-
+// NotLogged Error messsage when the user is not logged and cant access something
 const NotLogged = "Not Logged"
