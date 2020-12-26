89b37d62c31a88873c4ba5c370c07225c712f38f
Luiz Fernando <luizepauloxd@gmail.com>
2020-12-25 21:10:33 -0300

    feat: adds new constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,15 @@
 package constants
 
+const TweetDeleted = "The tweet has been deleted"
+
+const InternalServerError = "Internal Server Error"
+
+const NotFound = "Not Found"
+
 const WrongEmailOrPassword = "Wrong email or password"
 
 const UserAlreadyRegistered = "User already registered"
 
 const UnexpectedError = "An unexpected error occurred"
+
+const NotLogged = "Not Logged"
